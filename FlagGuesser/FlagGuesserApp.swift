2ddfc10d252c8e17c18dc810b3e18f8e3ebfbd0e
nusaibatron <nusaiba@berkeley.edu>
2022-08-12 10:45:31 -0400

    Initial Commit

---
+++ FlagGuesser/FlagGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuesserApp.swift
+//  FlagGuesser
+//
+//  Created by Nusaiba Rahman on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
